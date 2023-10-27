46eb86f422a90dc0bc0172b31da555a04f7c3615
Greyisheep <ibeawuchiclaret@gmail.com>
2023-10-27 08:31:25 -0700

    FastAPI setup

---
+++ house_prices_api/model_package/regression_model/pipeline.py
@@ -0,0 +1,45 @@
+from feature_engine.imputation import AddMissingIndicator
+from sklearn.linear_model import Lasso
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
+from model_package.regression_model.config.core import config
+from model_package.regression_model.processing import features as pp
+
+price_pipe = Pipeline(
+    [
+        (
+            "missing_indicator",
+            AddMissingIndicator(variables=config.model_config.numerical_vars_with_na),
+        ),
+        (
+            "mapper_some",
+            pp.Mapper(
+                variables=config.model_config.some_vars,
+                mappings=config.model_config.some_mappings,
+            ),
+        ),
+        (
+            "mapper_location",
+            pp.Mapper(
+                variables=config.model_config.location_vars,
+                mappings=config.model_config.location_mappings,
+            ),
+        ),
+        (
+            "mapper_finishing",
+            pp.Mapper(
+                variables=config.model_config.finishing_vars,
+                mappings=config.model_config.finishing_mappings,
+            ),
+        ),
+        ("scaler", MinMaxScaler()),
+        (
+            "Lasso",
+            Lasso(
+                alpha=config.model_config.alpha,
+                random_state=config.model_config.random_state,
+            ),
+        ),
+    ]
+)
---
+++ house_prices_api/model_package/regression_model/predict.py
@@ -0,0 +1,59 @@
+import typing as t
+
+import numpy as np
+import pandas as pd
+
+# from model_package.regression_model import __version__ as _version
+from model_package.regression_model.config.core import config
+from model_package.regression_model.processing.data_manager import load_pipeline
+# from regression_model.processing.validation import validate_inputs
+
+pipeline_file_name = f"{config.app_config.pipeline_save_file}.pkl"
+# pipeline_file_name = f"{config.app_config.pipeline_save_file}{_version}.pkl"
+_price_pipe = load_pipeline(file_name=pipeline_file_name)
+
+
+def make_prediction(
+    *,
+    input_data: t.Union[pd.DataFrame, dict],
+) -> dict:
+    """Make a prediction using a saved model pipeline."""
+
+    # data = pd.DataFrame(input_data)
+    # validated_data, errors = validate_inputs(input_data=data)
+    # # results = {"predictions": None, "version": _version, "errors": errors}
+    # results = {"predictions": None,"errors": errors}
+
+    # if not errors:
+    #     predictions = _price_pipe.predict(
+    #         X=validated_data[config.model_config.features]
+    #     )
+    #     results = {
+    #         "predictions": [np.exp(pred) for pred in predictions],  # type: ignore
+    #         # "version": _version,
+    #         "errors": errors,
+    #     }
+
+    # return results
+
+def make_prediction(
+    *,
+    input_data: t.Union[pd.DataFrame, dict],
+) -> dict:
+    """Make a prediction using a saved model pipeline."""
+
+    data = pd.DataFrame(input_data)
+    # validated_data, errors = validate_inputs(input_data=data)
+    # results = {"predictions": None, "version": _version, "errors": errors}
+    # results = {"predictions": None,"errors": errors}
+
+    predictions = _price_pipe.predict(
+        X=data[config.model_config.features]
+    )
+    results = {
+        "predictions": [np.exp(pred) for pred in predictions],  # type: ignore
+            # "version": _version,
+            # "errors": errors,
+    }
+
+    return results
