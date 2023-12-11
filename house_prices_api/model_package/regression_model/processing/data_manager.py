2bbd0d8c422ba162d6bcbe2cba166b966c74e8c4
unknown <ibeawuchiclaret@gmail.com>
2023-12-11 02:51:38 -0800

    Clean up code

--- house_prices_api/model_package/regression_model/processing/data_manager.py
+++ house_prices_api/model_package/regression_model/processing/data_manager.py
@@ -2,7 +2,9 @@ import os
 import sys
 
 # Add the project's root directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))
 
 # print(sys.path)
 
@@ -15,75 +17,7 @@ import pandas as pd
 from sklearn.pipeline import Pipeline
 
 from model_package.regression_model.config.core import TRAINED_MODEL_DIR, config
-
-def engineer_dataset() -> pd.DataFrame:
-    # Load data from the specified URL
-    url = config.model_config.url
-    dataframe = pd.read_csv(url)
-    
-    # Extract all unique utilities
-    all_utilities = set()
-    for row in dataframe['What utilities are available?']:
-        if isinstance(row, str):
-            utilities = [utility.strip() for utility in row.split(',')]
-            all_utilities.update(utilities)
-
-    # Create columns for each utility and initialize with False
-    for utility in all_utilities:
-        dataframe[utility] = False
-
-    # Update the columns based on the 'What utilities are available?' column
-    for index, row in dataframe.iterrows():
-        if pd.notna(row['What utilities are available?']):
-            utilities = [utility.strip() for utility in row['What utilities are available?'].split(',')]
-            for utility in utilities:
-                dataframe.at[index, utility] = True
-
-    # Drop the 'What utilities are available?' column
-    dataframe.drop('What utilities are available?', axis=1, inplace=True)
-
-    # Drop the 'Timestamp' column
-    dataframe = dataframe.drop('Timestamp', axis=1)
-    
-    dataframe.columns = ['Name', 'Rent', 'StrtName', 'Storeys', 'Cheaperflrs', 'LgCond', 'Age', 'GenHouse', 'Parking', 'Distance',
-       'Location', 'RdCond', 'SecurityLvl', 'RmSize', 'RmCond', 'Wdrobe',
-       'Finishing', 'Balcony', 'KitchenSize', 'BathrmSize', 'BalcnySize', 
-       'RefDisposal', 'LodgeGen', 'SecPost', 'Solar', 'RunWater', 'Cleaners',
-       'Electricity', 'ElecLodgeGen']
-
-    # Convert the "Rent" column to numeric (ignore errors for non-numeric values)
-    dataframe['Rent'] = pd.to_numeric(dataframe['Rent'], errors='coerce')
-
-    # Apply the condition to the "Rent" column using a mask
-    mask = (dataframe['Rent'].notna()) & (dataframe['Rent'] < 1000)
-
-    # Multiply the values that meet the condition by 1000
-    dataframe.loc[mask, 'Rent'] *= 1000
-
-    # Create a new 'ID' column with row IDs
-    dataframe['ID'] = range(1, len(dataframe) + 1)
-
-    # Reorder the columns so that 'ID' is the first column
-    dataframe = dataframe[['ID'] + [col for col in dataframe if col != 'ID']]
-
-    # Iterate through each column to handle NaN values
-    for col in dataframe.columns:
-        if dataframe[col].dtype in [int, float]:
-            # Replace NaN with the column's average
-            dataframe[col].fillna(dataframe[col].mean(), inplace=True)
-        elif dataframe[col].dtype == 'object':
-            # Replace NaN with the most common string value in the same column
-            most_common = dataframe[col].mode()[0]
-            dataframe[col].fillna(most_common, inplace=True)
-        elif dataframe[col].dtype == 'bool':
-            # Replace NaN with the most frequent boolean value
-            most_frequent = dataframe[col].mode()[0]
-            dataframe[col].fillna(most_frequent, inplace=True)
-    
-    transformed = dataframe.copy()
-
-    return transformed
-
+from processing.validation import engineer_dataset
 
 def save_pipeline(*, pipeline_to_persist: Pipeline) -> None:
     """Persist the pipeline.
@@ -120,3 +54,5 @@ def remove_old_pipelines(*, files_to_keep: t.List[str]) -> None:
     for model_file in TRAINED_MODEL_DIR.iterdir():
         if model_file.name not in do_not_delete:
             model_file.unlink()
+            
+transformed = engineer_dataset()
--- house_prices_api/model_package/regression_model/processing/validation.py
+++ house_prices_api/model_package/regression_model/processing/validation.py
@@ -6,6 +6,74 @@ from pydantic import BaseModel
 
 from model_package.regression_model.config.core import config
 
+def engineer_dataset() -> pd.DataFrame:
+    # Load data from the specified URL
+    url = config.model_config.url
+    dataframe = pd.read_csv(url)
+    
+    # Extract all unique utilities
+    all_utilities = set()
+    for row in dataframe['What utilities are available?']:
+        if isinstance(row, str):
+            utilities = [utility.strip() for utility in row.split(',')]
+            all_utilities.update(utilities)
+
+    # Create columns for each utility and initialize with False
+    for utility in all_utilities:
+        dataframe[utility] = False
+
+    # Update the columns based on the 'What utilities are available?' column
+    for index, row in dataframe.iterrows():
+        if pd.notna(row['What utilities are available?']):
+            utilities = [utility.strip() for utility in row['What utilities are available?'].split(',')]
+            for utility in utilities:
+                dataframe.at[index, utility] = True
+
+    # Drop the 'What utilities are available?' column
+    dataframe.drop('What utilities are available?', axis=1, inplace=True)
+
+    # Drop the 'Timestamp' column
+    dataframe = dataframe.drop('Timestamp', axis=1)
+    
+    dataframe.columns = ['Name', 'Rent', 'StrtName', 'Storeys', 'Cheaperflrs', 'LgCond', 'Age', 'GenHouse', 'Parking', 'Distance',
+       'Location', 'RdCond', 'SecurityLvl', 'RmSize', 'RmCond', 'Wdrobe',
+       'Finishing', 'Balcony', 'KitchenSize', 'BathrmSize', 'BalcnySize', 
+       'RefDisposal', 'LodgeGen', 'SecPost', 'Solar', 'RunWater', 'Cleaners',
+       'Electricity', 'ElecLodgeGen']
+
+    # Convert the "Rent" column to numeric (ignore errors for non-numeric values)
+    dataframe['Rent'] = pd.to_numeric(dataframe['Rent'], errors='coerce')
+
+    # Apply the condition to the "Rent" column using a mask
+    mask = (dataframe['Rent'].notna()) & (dataframe['Rent'] < 1000)
+
+    # Multiply the values that meet the condition by 1000
+    dataframe.loc[mask, 'Rent'] *= 1000
+
+    # Create a new 'ID' column with row IDs
+    dataframe['ID'] = range(1, len(dataframe) + 1)
+
+    # Reorder the columns so that 'ID' is the first column
+    dataframe = dataframe[['ID'] + [col for col in dataframe if col != 'ID']]
+
+    # Iterate through each column to handle NaN values
+    for col in dataframe.columns:
+        if dataframe[col].dtype in [int, float]:
+            # Replace NaN with the column's average
+            dataframe[col].fillna(dataframe[col].mean(), inplace=True)
+        elif dataframe[col].dtype == 'object':
+            # Replace NaN with the most common string value in the same column
+            most_common = dataframe[col].mode()[0]
+            dataframe[col].fillna(most_common, inplace=True)
+        elif dataframe[col].dtype == 'bool':
+            # Replace NaN with the most frequent boolean value
+            most_frequent = dataframe[col].mode()[0]
+            dataframe[col].fillna(most_frequent, inplace=True)
+    
+    transformed_data = dataframe.copy()
+
+    return transformed_data
+
 
 def drop_na_inputs(*, input_data: pd.DataFrame) -> pd.DataFrame:
     """Check model inputs for na values and filter."""
